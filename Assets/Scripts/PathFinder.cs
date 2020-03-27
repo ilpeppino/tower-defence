eee0d4eb8617e571a4959b6c8131eb3bc0559437
ilpeppino <ilpeppino@gmail.com>
2020-03-27 15:42:56 +0100

    137 - BSF working with finding the end waypoint!

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -8,7 +8,7 @@ public class PathFinder : MonoBehaviour
     // Maps each cube (waypoint) in path with its position in the world
     private Dictionary<Vector2Int, Waypoint> _grid = new Dictionary<Vector2Int, Waypoint>();
 
-    // List of waypoints (cubes) defined in Path
+    // Collection of all the waypoints (cubes) defined in Path
     private Waypoint[] _waypoints;
 
     // Used to explore neighbor waypoints (cubes)
@@ -18,17 +18,20 @@ public class PathFinder : MonoBehaviour
                                         Vector2Int.left
                                      };
 
+    // Used for path finding algorithm
+    private Queue<Waypoint> _queue = new Queue<Waypoint>();
 
-
-    [SerializeField] private Waypoint _startCube;
-    [SerializeField] private Waypoint _endCube;
+    [SerializeField] private bool _isEndNodeFound;
+    [SerializeField] private Waypoint _startWaypoint;
+    [SerializeField] private Waypoint _endWaypoint;
+    
 
     private void Awake()
     {
         LoadAllBlocks();
         GenerateDictionary();
         ColorStartEndBlocks();
-        ExploreNeighbors();
+        FindPath();
     }
 
     private void LoadAllBlocks()
@@ -38,8 +41,8 @@ public class PathFinder : MonoBehaviour
 
     private void ColorStartEndBlocks()
     {
-        _startCube.SetWaypointColor(Color.green);
-        _endCube.SetWaypointColor(Color.red);
+        _startWaypoint.SetWaypointColor(Color.green);
+        _endWaypoint.SetWaypointColor(Color.red);
     }
 
     private void GenerateDictionary()
@@ -53,24 +56,67 @@ public class PathFinder : MonoBehaviour
         }
     }
 
-    private void ExploreNeighbors()
+
+
+
+    private void FindPath()
     {
+        // Initializes the pathfinding markers variables
+        var _currentWaypoint = _startWaypoint;
+        _queue.Enqueue(_startWaypoint);
 
-        foreach (Waypoint _waypoint in _waypoints)
+        // Iterates as long as the queue is not empty or the end node is found
+        while(_queue.Count > 0 && !isEndReached(_currentWaypoint))
         {
-            foreach (Vector2Int _direction in _compass)
+            // Picks up the first waypoint from the queue
+            _currentWaypoint = _queue.Dequeue();
+            Debug.Log(_currentWaypoint + " - dequeued");
+
+            // Checks if that waypoint is the end waypoint
+            if (!isEndReached(_currentWaypoint))
             {
-                print("WAYPONT " + _waypoint);
+                // Enqueues all its neighbor waypoints
+                ExploreNeighbors(_currentWaypoint);
 
-                Vector2Int neighbor = _waypoint.GetGridPos() + _direction;
-                print("Neighbor: " + neighbor);
-
-            } 
-            print("------------------------------");
+            }
         }
 
+        Debug.Log(_currentWaypoint + " - node found");
 
 
+
+    }
+
+    private void ExploreNeighbors(Waypoint _waypoint)
+    {
+        // Iterates over the 4 directions (NSWE) and enqueues the waypoints 
+        foreach (Vector2Int _direction in _compass)
+        {
+            var _waypointKey = _waypoint.GetGridPos() + _direction;
+            try
+            {
+                var _waypointValue = _grid[_waypointKey];
+                _waypointValue = _grid[_waypointKey];
+                _waypointValue.SetWaypointColor(Color.white);
+                _queue.Enqueue(_waypointValue);
+                Debug.Log(_waypointValue + " - queued");
+            }
+            
+            catch
+            {
+                Debug.LogWarning(_waypointKey + " - do not exist");
+            }
+            
+            
+            
+        }
+
+    }
+
+    private bool isEndReached(Waypoint _currentWaypoint)
+    {
+        return (_currentWaypoint == _endWaypoint);
+        
     }
 
 
